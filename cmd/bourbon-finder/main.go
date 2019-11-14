85019c8fd71a26f66c6aade3b761934adaf59fd5
Austin Keeley <austin.keeley@gmail.com>
2019-11-13 19:18:59 -0500

    Adding command line flags

--- cmd/bourbon-finder/main.go
+++ cmd/bourbon-finder/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"flag"
+	"os"
 	"bourbonfinder"
-	"log"
 )
 
 func main() {
-	log.Println("Bourbon Finder")
-	bourbonfinder.Search("data/config.json")
+
+	var configFileName string
+	flag.StringVar(&configFileName, "config", "", "Path to the config JSON file. Required.")
+	flag.StringVar(&configFileName,"c", "", "Path to the config JSON file (shorthand). Required.")
+	flag.Parse()
+
+	if configFileName == "" {
+		usage()
+		os.Exit(1)
+	}
+
+	//bourbonfinder.Search("data/config.json")
+	bourbonfinder.Search(configFileName)
+}
+
+func usage() {
+	fmt.Println("Usage: bourbon-finder <options>")
+	flag.PrintDefaults()
 }
